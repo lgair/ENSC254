a8c89eb340516a357f768f10fdf7516fbdd09157
Laika <laika.catt@gmail.com>
2018-06-08 13:01:01 -0700

    final version 64b numbers working

--- LAB3/sqrt.s
+++ LAB3/sqrt.s
@@ -1,35 +1,38 @@
 .global _start
 _start:
-    ;@12345678^2 into r0 & r1
-    ldr r0, =#35487  	;@ MSB of 64b num here
-	ldr r1, =#260846532 ;@ LSB of 64b num here
-    mov r2, #0  	    ;@ Initialize and store calculated root
-    ldr r3, =#4294967295;@ Largest number that can fit in a register (2^32)-1
-    mov r4, #0 		    ;@ LSB for UMULL
-    mov r5, #0 		    ;@ MSB for UMULL
-    rrx r6, r3 		    ;@ midpoint starting at 1/2 LB
+    @; 64b number into r0 and r1
+    ldr r0, =#35487         ;@ MSB of 64b num here		
+    ldr r1, =#260846532		;@ LSB of 64b num here
+    ldr r3, =#4294967295	;@ Max num of 32b register 
+    mov r6, r3, lsr #1		;@ r6 is midpoint of our set 
+    mov r8, #0			    ;@ Midpoint check
 isRoot:
-    umull r4, r5, r6, r6    ;@ Multiply the midpoint by itself, break into two registers
-    cmp r0, r5              ;@ compare MSB registers of the multipliication
-    bhi isSmaller
-    bls isLarger
-    cmp r1, r4              ;@ Compare LSB registers of the multiplication
-    bhi isSmaller
-    bls isLarger
-    beq Root                ;@if we get here we know we have a root
+    umull r4, r5, r6, r6	;@ r4 - LSB of umull, r5 - MSB of umull
+    cmp r0, r5			    ;@ Compare MSB's
+    blo isSmaller
+    bhi isLarger
+    cmp r1, r4			    ;@ Compare LSB's
+    blo isSmaller
+    bhi isLarger
+    beq Root			    ;@ if we get here, we have a root
 isSmaller:
-    sub r7, r3, r6  ;@ subtract the midpoint from the largest possible 32b number
-    mov r4, r6      ;@ move the old midpoint to be the new max 32b number
-    rrx r7, r7      ;@ calculate new midpoint
-    sub r6, r6, r7  ;@ reinstate new midpoint over old one
+    sub r7, r3, r6		    ;@ Subtract midpoint from our total set
+    mov r3, r6			    ;@ Move old midpoint to be new max
+    mov r7, r7, lsr #1		;@ Find new midpoint 
+    cmp r7, r8			    ;@ Check that the new midpoint != 0
+    addeq r7, r7, #1
+    sub r6, r6, r7		    ;@ Place new midpoint back into r6
     b isRoot
-isLarger:           ;@ Logic is the same as isSmall, save operations at the end are inverted
-    sub r7, r3, r6
-    mov r4, r6
-    rrx r7, r7
+isLarger:			        ;@ Logic same as isSmaller save inverted operations at end
+    sub R7, R3, R6
+    mov R3, r6
+    mov r7, r7, lsr #1
+    cmp r7, r8
+    subeq r7, r7, #1
     add r6, r6, r7
     b isRoot
 Root:
-    mov r2, r4
-End:
-    b End
+    mov r2, r6			    ;@ Store midpoint
+fin:
+    b fin
+
