ab8d4889648b3bd6349653ae2bd7872f29994ca8
Laika <laika.catt@gmail.com>
2018-06-08 13:06:29 -0700

    lsr -> rrx rn, rm

--- LAB3/sqrt.s
+++ LAB3/sqrt.s
@@ -4,7 +4,7 @@ _start:
     ldr r0, =#35487         ;@ MSB of 64b num here		
     ldr r1, =#260846532		;@ LSB of 64b num here
     ldr r3, =#4294967295	;@ Max num of 32b register 
-    mov r6, r3, lsr #1		;@ r6 is midpoint of our set 
+    rrx r6, r3		        ;@ r6 is midpoint of our set 
     mov r8, #0			    ;@ Midpoint check
 isRoot:
     umull r4, r5, r6, r6	;@ r4 - LSB of umull, r5 - MSB of umull
@@ -18,7 +18,7 @@ isRoot:
 isSmaller:
     sub r7, r3, r6		    ;@ Subtract midpoint from our total set
     mov r3, r6			    ;@ Move old midpoint to be new max
-    mov r7, r7, lsr #1		;@ Find new midpoint 
+    rrx r7, r7		        ;@ Find new midpoint 
     cmp r7, r8			    ;@ Check that the new midpoint != 0
     addeq r7, r7, #1
     sub r6, r6, r7		    ;@ Place new midpoint back into r6
@@ -26,7 +26,7 @@ isSmaller:
 isLarger:			        ;@ Logic same as isSmaller save inverted operations at end
     sub R7, R3, R6
     mov R3, r6
-    mov r7, r7, lsr #1
+    rrx r7, r7
     cmp r7, r8
     subeq r7, r7, #1
     add r6, r6, r7
